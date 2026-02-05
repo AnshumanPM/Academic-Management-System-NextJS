d447fd02978b6f9f4f62743808b841d5e55c4e0b
AnshumanPM <anshumanprasadmahanta@gmail.com>
2026-02-05 20:20:43 +0530

    fix env

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,7 +1,7 @@
 // instrumentation-client.js
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST as string,
   defaults: "2025-11-30",
 });
